8a6c2557d4368fe8e066d22a79b57697cc93cbb4
B <>
2024-11-21 17:51:23 +0700

    add project

---
+++ event-manager/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is a React app with TypeScript!</p>
+    </div>
+  );
+};
+
+export default App;
