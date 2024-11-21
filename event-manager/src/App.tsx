10aa72ef56327489e7dca1d876685fd5cc5562dc
Linh Duy <97662709+linhduy2192001@users.noreply.github.com>
2024-11-21 23:28:11 +0700

    ui

--- event-manager/src/App.tsx
+++ event-manager/src/App.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-import MainLayout from "./layouts/MainLayout";
-import AuthLayout from "./layouts/AuthLayout";
-import DashboardLayout from "./layouts/DashboardLayout";
+import MainLayout from "./Layouts/MainLayout";
+import AuthLayout from "./Layouts/AuthLayout";
+import DashboardLayout from "./Layouts/DashboardLayout";
+import Login from "./pages/Login/LoginPage";
 
 // Import pages
-import RegisterPage from "./pages/RegisterPage";
-import HomePage from "./pages/HomePage";
-import EventDetailsPage from "./pages/EventDetailsPage";
-import UserDashboardPage from "./pages/UserDashboardPage";
-import AdminDashboardPage from "./pages/AdminDashboardPage";
-import SpeakerManagementPage from "./pages/SpeakerManagementPage";
-import CheckInPage from "./pages/CheckInPage";
-import SchedulePage from "./pages/SchedulePage";
-import StatisticsPage from "./pages/StatisticsPage";
-import ForumPage from "./pages/ForumPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import HomePage from "./pages/HomePage";
+// import EventDetailsPage from "./pages/EventDetailsPage";
+// import UserDashboardPage from "./pages/UserDashboardPage";
+// import AdminDashboardPage from "./pages/AdminDashboardPage";
+// import SpeakerManagementPage from "./pages/SpeakerManagementPage";
+// import CheckInPage from "./pages/CheckInPage";
+// import SchedulePage from "./pages/SchedulePage";
+// import StatisticsPage from "./pages/StatisticsPage";
+// import ForumPage from "./pages/ForumPage";
 
 const App: React.FC = () => {
   return (
@@ -24,25 +24,25 @@ const App: React.FC = () => {
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
+          {/* <Route index element={<HomePage />} />
           <Route path="event/:id" element={<EventDetailsPage />} />
           <Route path="schedule" element={<SchedulePage />} />
-          <Route path="forum" element={<ForumPage />} />
+          <Route path="forum" element={<ForumPage />} /> */}
         </Route>
 
         {/* Authentication Routes */}
         <Route path="/auth" element={<AuthLayout />}>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="register" element={<RegisterPage />} /> */}
         </Route>
 
         {/* Dashboard Routes */}
         <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="user" element={<UserDashboardPage />} />
+          {/* <Route path="user" element={<UserDashboardPage />} />
           <Route path="admin" element={<AdminDashboardPage />} />
           <Route path="speakers" element={<SpeakerManagementPage />} />
           <Route path="check-in" element={<CheckInPage />} />
-          <Route path="statistics" element={<StatisticsPage />} />
+          <Route path="statistics" element={<StatisticsPage />} /> */}
         </Route>
       </Routes>
     </Router>
